c89a1be665fcfe1da0960fbead269c0e2da8aaee
liziwl <leezisy@gmail.com>
2020-06-30 14:58:50 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,20 +12,15 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Blog', link: '/blog/' },
+      { text: 'Blog', link: 'https://liziwl.cn/blog' },
     ],
-    repo: 'liziwl.github.io',
-    docsRepo: 'liziwl.github.io',
-    // editLinkText: '一起完善这本手册！',
-    docsDir: 'docs',
-    // repoLabel: '在Github上查看',
+    repo: 'https://github.com/liziwl',
+
     editLinks: false,
     smoothScroll: true,
-    // sidebarDepth: 2,
     sidebar: false,
     search: false,
     nextLinks: false,
-    // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
-    prevLinks: false
+    prevLinks: false // 默认值是 true
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,19 @@
-# Hello VuePress
+# Home
+
+## Biography
+
+<img src="./avatar.jpg" alt="protrait" width="200" style="margin-top:5px">
+
+Ziqiang Li （李子强）
+
+Email: 11930674__AT__mail.sustech.edu.cn
+
+## Education
+
+
+## Publications
+
+* \[ICSE 2020\] Collaborative bug finding for Android apps
+* \[ICSE 2020 Poster\] Bugine: a bug report recommendation system for Android apps
+  * [\[Poster\]](./ziqiang-icse2020-poster.pdf), [\[Video\]](https://youtu.be/v9qcGVhUIyI)
+
